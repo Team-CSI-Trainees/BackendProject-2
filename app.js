47fdf878fafc71318d4066edf2d178dcbe785c10
2111091 <anshikapurilearner@gmail.com>
2022-11-02 23:16:35 -0700

    second commit

--- app.js
+++ app.js
@@ -53,7 +53,13 @@ app.post("/compose", function(req, res){
     title: req.body.postTitle,
     content: req.body.postBody
   });
-post.save();
+  post.save(function(err){
+    if(!err){
+      res.redirect("/");
+    }
+  });
+});
+
 app.get("/posts/:postName", function(req, res){
   const requestedTitle = _.lowerCase(req.params.postName);
 
